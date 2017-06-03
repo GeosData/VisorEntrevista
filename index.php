e01ef857acb69cc368e7c71aff9fb50728c3ab18
Jose Tirado <jotive@gmail.com>
2017-06-03 09:57:00 -0500

    Estructura basica del proyecto

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Visor de Entrevistas</title>
+</head>
+<body>
+
+<header>
+	<a href="ingreso.php">Ingreso</a>
+</header>
+<div id="logo">
+	<h1>Visor de Entrevistas</h1>
+</div>
+<div id="calendario">
+	
+</div>
+</body>
+</html>
---
+++ ingreso.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ingreso a plataforma</title>
+</head>
+<body>
+
+</body>
+</html>
